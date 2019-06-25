a38bed6319251988b9bca1a3c5c5ed57202a4451
8wgf3b <karnabalaj@gmail.com>
2019-06-25 18:36:43 +0530

    Setting up for aws

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('noice')
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def api():
+    return 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
