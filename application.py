fd3cd18153479d8e16e612cb1b058dec0efa14c1
8wgf3b <karnabalaj@gmail.com>
2019-07-01 15:10:09 +0530

    basic setup for heroku linking

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
 from flask import Flask, request, Response
+from utils import clean
+import os
 import requests
+import json
 
 application = app = Flask(__name__)
 
 @app.route('/small', methods=['POST', 'GET'])
 def api():
-    return 'hmmm'
+    if request.method == 'POST':
+        js = request.get_json()
+        if js['func'] == '/clam':
+            mess = {'result':clean(log=True)}
+            return json.dumps(mess)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
