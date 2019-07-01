2f7027d52e7a5bea4fb6e64696bde4f1c61e837e
8wgf3b <karnabalaj@gmail.com>
2019-07-01 23:23:10 +0530

    fixed import errors

--- reddit.py
+++ reddit.py
@@ -1,11 +1,15 @@
 import praw
-import os
+import os, shutil
 import requests
 from utils import zipit, clean, timeformat, utc
+import urllib
 
 def redditusermedia(user):
     address = os.environ['REDDITAPI']
     bigbro = os.environ['OWNNAME']
+
+#    address = 'https://api.pushshift.io/reddit/search'
+#    bigbro = ''
     user = user.split('/')[-1]
     r = requests.get(address+'submission/?'+'author='+ user +'&fields=url,title,created_utc&size=500')
     js = r.json()['data']
@@ -31,9 +35,12 @@ def redditusermedia(user):
                 ext = x.split('.')[-1]
                 urllib.request.urlretrieve(x, directory +'/' + dic[x] + '.' + ext)
         except Exception as e:
+            print(x)
+            print(e)
             continue
     path = zipit(directory).split('/')[-1]
-    mainbody = user + '\n\n' + 'ziploc\n' + bigbro + 'temp/' + path + '\n\n' + mainbody
+    shutil.rmtree(directory)
+    mainbody = user + '\n\n' + 'ziploc\n' + bigbro + 'temp/' + path + '\n\n'
     return mainbody
 
 
