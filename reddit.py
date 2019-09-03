7c0e6973c312dbfebc52062af53bdf191e3560c7
8wgf3b <karnabalaj@gmail.com>
2019-09-04 04:48:09 +0530

    Make redit.py standalone script

--- reddit.py
+++ reddit.py
@@ -1,14 +1,58 @@
-import praw
 import os, shutil
 import requests
 from utils import zipit, clean, timeformat, utc
 import urllib
+from sys import argv
+import calendar
+from datetime import datetime, date
+
+def timeformat(x=datetime.now(), typ='DATE'):
+    if type(x) != type(datetime.now()):
+        x = datetime(x)
+    if typ == 'DATE':
+        return x.strftime('%Y-%m-%d')
+    elif typ =='HOUR':
+        return x.strftime('%H:%M:%S')
+    elif typ=='BOTH':
+        return x.strftime('%Y-%m-%d')+'%20'+x.strftime('%H:%M:%S')
+
+def utc(x, totimestamp=False):
+    if totimestamp == False:
+        return timeformat(datetime.utcfromtimestamp(x))
+    else:
+        d = datetime(*x)
+        epoch = datetime(1970,1,1)
+        return int((d - epoch).total_seconds())
+
+
+def zipit(path):
+    try:
+        shutil.make_archive(path, 'zip', path)
+        return path + '.zip'
+    except Exception as e:
+        print(e)
+
+def clean(path = 'temp/', log = False):
+    for file in os.listdir(path):
+        if file == '.gitkeep':
+            continue
+        file_path = os.path.join(path, file)
+#        print(file_path)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+    if log == True:
+        return '\n'.join(os.listdir(path))
 
 def redditusermedia(user):
-#    address = os.environ['REDDITAPI']
-#    bigbro = os.environ['OWNNAME']
-    address = 'https://api.pushshift.io/reddit/search/'
-    bigbro = ''
+    address = os.getenv('REDDITAPI', 'https://api.pushshift.io/reddit/search/')
+    bigbro = os.getenv('OWNNAME', '')
+#    address = 'https://api.pushshift.io/reddit/search/'
+#    bigbro = ''
     user = user.split('/')[-1]
     r = requests.get(address+'submission/?'+'author='+ user +'&fields=url,title,created_utc&size=500')
     js = r.json()['data']
@@ -44,4 +88,7 @@ def redditusermedia(user):
 
 
 if __name__ == '__main__':
-    print(redditusermedia('bbypocahontas'))
+    #print(redditusermedia('opiumzxq'))
+    for item in argv[1:]:
+        print(redditusermedia(item))
+    print('\n\nThe zipped files are stored in temp folder')
